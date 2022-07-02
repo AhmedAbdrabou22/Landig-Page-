bf1279bf1a8719610afa6676ea5e01f9f79a12e1
Ahmed Abdrabou <101427863+AhmedAbdrabou22@users.noreply.github.com>
2022-07-02 11:09:32 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,33 @@
 $(document).ready(function(){
     $('.owl-carousel').owlCarousel();
 });
+var setting = document.getElementById("setting");
+
+setting.addEventListener("click",()=>{
+    if(setting.classList.contains('toggle')){
+        setting.classList.remove('toggle')
+        setting.classList.add('sett');
+        setting.style.transition = '1s';
+    }else{
+        setting.classList.add('toggle');
+    }
+})
+
+//Calling Bakground colors for change color of background 
+var color1 = document.querySelector('.color1')
+var color2 = document.querySelector('.color2')
+var color3 = document.querySelector('.color3')
+var color4 = document.querySelector('.color4')
+
+var svg = document.querySelector('.svg path');
+var nav = document.querySelector('.nav');
+var test = document.querySelector('.test .overlay');
+var sub = document.querySelector('.subscripe .overlay');
+var footer = document.querySelector('.footer')
+color3.addEventListener('click',()=>{
+    svg.setAttribute('fill','#05b3b6');
+    nav.style.backgroundColor ='#05b3b6';
+    test.style.backgroundColor ='#05b3b6';
+    sub.style.backgroundColor ='#05b3b6';
+    footer.style.backgroundColor ='#05b3b6';
+})
