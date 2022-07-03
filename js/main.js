a3995b17b59e867362255060ff553d95277aa08b
Ahmed Abdrabou <101427863+AhmedAbdrabou22@users.noreply.github.com>
2022-07-03 14:49:41 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -14,20 +14,25 @@ setting.addEventListener("click",()=>{
 })
 
 //Calling Bakground colors for change color of background 
-var color1 = document.querySelector('.color1')
-var color2 = document.querySelector('.color2')
-var color3 = document.querySelector('.color3')
-var color4 = document.querySelector('.color4')
+
 
 var svg = document.querySelector('.svg path');
 var nav = document.querySelector('.nav');
 var test = document.querySelector('.test .overlay');
 var sub = document.querySelector('.subscripe .overlay');
 var footer = document.querySelector('.footer')
-color3.addEventListener('click',()=>{
-    svg.setAttribute('fill','#05b3b6');
-    nav.style.backgroundColor ='#05b3b6';
-    test.style.backgroundColor ='#05b3b6';
-    sub.style.backgroundColor ='#05b3b6';
-    footer.style.backgroundColor ='#05b3b6';
+var features = document.querySelector('.features')
+var caurs = document.querySelector('.caurs')
+var allColors = document.querySelectorAll('.color');
+
+allColors.forEach((color)=>{
+    color.addEventListener('click',()=>{
+        var atr = color.getAttribute('value');
+        nav.style.backgroundColor=atr;
+        svg.setAttribute('fill',atr);
+        test.style.backgroundColor=atr;
+        sub.style.backgroundColor=atr;
+        footer.style.backgroundColor=atr;
+        features.style.backgroundColor=atr;
+    })
 })
